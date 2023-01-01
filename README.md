8614042ccd771a709d5a20e7909642b9682f1a83
Daviafer <daviafer1@gmail.com>
2023-01-01 19:38:12 -0300

    Finalizado o JavaScript e partes do README

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# Gerador Senha
+<h1 align="center"> Gerador senha</h1>
+
+## Sobre:
+Decorre de estudos do FrontEnd. <br />
+Projeto que tem a finalidade de gerar senhas, caso o usuário deseja, ao realizar o cadastro no formulário.
+
+## Ilustração:
+ <img alt="Gerando a senha. Gif ilustrativo do projeto" src="" width="400px">
+
+## Tecnologias utilizadas:
+- HTML | CSS | JavaScript
+
+## Possível melhoria(s):
+* Colocar um filtro na escolha de como poderá ser a senha aleatória.
+
+[Para visualizar o projeto: Clique aqui!]()
--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,52 @@
 // Seleção de Elementos
-const generatePasswordButton = document.querySelector("generate-password")
-const generatedPasswordElement = document.querySelector("#generated-password")
+const generatePasswordButton = document.querySelector("#generate-password");
+const generatedPasswordElement = document.querySelector("#generated-password");
 
 // Funções
+const getLetterLowerCase = () => {
+  return(String.fromCharCode(Math.floor(Math.random() * 26) + 97));
+};
+const getLetterUpperCase = () => {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+};
+const getNumber = () => {
+  return Math.floor(Math.random() * 10).toString();
+};
+const getSymbol = () => {
+  const symbols = "!@#$%&*()_-+=-*/[]{}~^";
+  return symbols[Math.floor(Math.random() * symbols.length)];
+};
+// console.log(getLetterLowerCase());
+const generatePassword = (getLetterLowerCase, getLetterUpperCase, getNumber, getSymbol) => {
+  let password = "";
+  const passwordLenght = 10;
+  const generators = [
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  ];
+  for(i = 0; i < passwordLenght; i = i + 4){
+    generators.forEach(() => {
+      const randomValue =
+      generators[Math.floor(Math.random() * generators.length)]();
 
+      password += randomValue;
+    });
+  }
+  password = password.slice(0, passwordLenght);
+  console.log(password);
+
+  generatedPasswordElement.style.display = "block";
+  generatedPasswordElement.querySelector("h4").innerText = password;
+};
 
 // Eventos
 generatePasswordButton.addEventListener("click", () => {
-  console.log("teste")
-})
+  generatePassword(
+    getLetterLowerCase,
+    getLetterUpperCase,
+    getNumber,
+    getSymbol,
+  );
+});
